5069cfee00c93bc2ab217d5df76e9ad625b7a7e7
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-02-16 22:39:26 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const toggles = document.querySelectorAll('.faq-toggle');
+
+	toggles.forEach((toggle) => {
+		toggle.addEventListener('click', () => {
+			const answer = toggle.nextElementSibling;
+			const icon = toggle.querySelector('.faq-icon');
+			answer.classList.toggle('active');
+
+			// Change the icon from '+' to '-' and vice versa
+			if (answer.classList.contains('active')) {
+				icon.textContent = '-';
+			} else {
+				icon.textContent = '+';
+			}
+		});
+	});
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	const hamburger = document.getElementById('hamburger');
+	const navbar = document.getElementById('navbar');
+	const close = document.getElementById('close');
+
+	const toggleNavbar = () => {
+		const isNavbarOpen = navbar.style.left === '0px';
+		navbar.style.left = isNavbarOpen ? '-100%' : '0px';
+	};
+
+	hamburger.addEventListener('click', toggleNavbar);
+	close.addEventListener('click', toggleNavbar);
+});
+
+function toggleModal(show) {
+	var modal = document.getElementById('myModal');
+	modal.style.display = show ? 'block' : 'none';
+}
---
+++ sources.md
@@ -0,0 +1,5 @@
+hero: https://unsplash.com/photos/brown-wooden-dock-on-blue-sea-under-blue-sky-during-daytime-6xoO8HfIrlE
+test3: https://unsplash.com/photos/water-falls-on-forest-hjW4o5XfCzc
+test2: https://unsplash.com/photos/a-scenic-view-of-a-town-and-the-ocean-jcIfOeRn3cw
+test1: https://unsplash.com/photos/brown-wooden-chairs-on-brown-wooden-dock-near-body-of-water-during-daytime-rIhcqUebiGw
+footerImage: https://unsplash.com/photos/brown-nipa-hut-on-white-sand-beach-during-daytime-4aqH2utAPAs
